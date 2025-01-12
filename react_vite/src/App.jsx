7ed0cfe1b1c0ed1b090174540df30668f240f8a6
arun-198 <n.arunalagappan@gmail.com>
2025-01-12 23:48:01 +0800

    Created new vite-react project for a finance tracker

---
+++ react_vite/src/App.jsx
@@ -0,0 +1,120 @@
+
+// import './App.css'
+// import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+// import DashboardIcon from '@mui/icons-material/Dashboard';
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DescriptionIcon from '@mui/icons-material/Description';
+// import LayersIcon from '@mui/icons-material/Layers';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { useDemoRouter } from '@toolpad/core/internal';
+import AddIcon from '@mui/icons-material/Add';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+
+
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: 'home',
+    title: 'Home',
+    icon: <DescriptionIcon />,
+  },
+  {
+    segment: 'expenses',
+    title: 'Add Expense',
+    icon: <AddIcon />,
+  },
+  {
+    segment: 'inflow',
+    title: 'Inflow',
+    icon: <AttachMoneyIcon />,
+  },
+  {
+    kind: 'divider',
+  },
+  {
+    kind: 'header',
+    title: 'Analytics',
+  },
+  {
+    segment: 'dashboard',
+    title: 'Dashboard',
+    icon: <BarChartIcon />,
+    
+  },
+  
+];
+
+const demoTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: 'data-toolpad-color-scheme',
+  },
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function DemoPageContent({ pathname }) {
+  return (
+    <Box
+      sx={{
+        py: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Typography>Dashboard content for {pathname}</Typography>
+    </Box>
+  );
+}
+
+DemoPageContent.propTypes = {
+  pathname: PropTypes.string.isRequired,
+};
+
+function App() {
+
+  const router = useDemoRouter('/dashboard');
+
+
+
+  return (
+    <>
+      <AppProvider
+        navigation={NAVIGATION}
+        branding={{
+          logo:  <AccountBalanceIcon style={{marginTop:'25%',fill: 'var(--mui-palette-primary-main)'}}/>,
+          title: 'Finance Tracker',
+          homeUrl: '/home',
+        }}
+        router={router}
+        theme={demoTheme}
+      >
+        
+        <DashboardLayout> 
+          <DemoPageContent pathname={router.pathname} />
+        </DashboardLayout>
+      </AppProvider>
+    </>
+  )
+}
+
+export default App
---
+++ static_server_build/main.js
@@ -0,0 +1,4 @@
+import classes from "./style.module.css"
+console.log("Hello World!");
+
+document.querySelector("H1").className = classes.title;
